247505157b528d4ff49cad3028fb6f0c62f34b9a
fahri <fahrumfahriansyah1@gmail.com>
2024-04-20 19:09:12 +0700

    bookshelf Api

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+const {addBooksHandler,getBooksHandler,getBooksByIdHandler,editBooksByIdHandler,deleteBooksByIdHandler} =require("./handler")
+
+const routes = [
+  {
+        method: 'POST',
+        path: '/books',
+        handler: addBooksHandler
+    },
+    {
+                method: 'GET',
+                path: '/books',
+                handler: getBooksHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books/{bookId}',
+        handler: getBooksByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/books/{bookId}',
+        handler:editBooksByIdHandler,
+      },
+      {
+        method: 'DELETE',
+        path: '/books/{bookId}',
+        handler: deleteBooksByIdHandler,
+      }, 
+];
+
+
+module.exports = routes;
